37493d1924124ab5b53ab142689a3f478005b269
Mayank <mayankjod016@gmail.com>
2025-05-07 16:51:21 +0530

    Initialised Repo

--- src/components/landing_page/Footer.jsx
+++ src/components/landing_page/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center py-4 text-sm text-gray-100 bg-black">
+    <footer className="text-center py-3 text-sm text-gray-100 bg-black">
       © 2025 WritingArena. Built by Mayank.
     </footer>
   );
