fc1596963a67d60b4a3c8e89e969ed77648534c7
Mayank <mayankjod016@gmail.com>
2025-05-17 16:09:36 +0530

    added article set page and made some changes to dashboard

--- src/app/articles/page.js
+++ src/app/articles/page.js
@@ -1,42 +1,24 @@
-import { Button } from "../../components/ui/button";
 import { Card, CardDescription, CardTitle } from "../../components/ui/card";
-import { Avatar, AvatarImage } from "../../components/ui/avatar";
 import Footer from "@/components/landing_page/Footer";
 import NavigationMenu from "@/components/general/navigation-menu";
+import ArticleSet from "@/components/general/article-set";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <div className="flex-grow overflow-auto">
         {/* Navigation menu */}
-        <NavigationMenu/>
+        <NavigationMenu />
         <div className="mx-5 mt-10 flex relative text-white gap-5">
           {/*daily topic card */}
           <div className="flex-3/4">
-            <div className=" absolute -top-4 left-4 bg-black border-2 border-amber-50 text-4xl text-amber-50 font-mono font-bold px-5 py-1 rounded-2xl">
-              Topic of the day
-            </div>
-            <Card className="mt-2 w-280 text-white bg-zinc-900 border-2 px-10 border-amber-50">
-              <p className="text-3xl pt-2">If I could time travel...</p>
-              <div className="flex-grow border-t border-gray-300"></div>
-              <CardDescription className="text-2xl text-zinc-300">
-                description text and links for the research purposes.
-              </CardDescription>
-              <Button
-                variant="ghost"
-                size="lg"
-                className="font-mono font-bold text-2xl border-1 w-80 ml-auto hover:cursor-pointer"
-              >
-                Start Writing
-              </Button>
-            </Card>
+            <ArticleSet/>
           </div>
 
           {/* Right part */}
           <div className="flex-1/4 mt-2 flex flex-col gap-5">
             <Card className="text-white bg-zinc-900 border-2 px-10 border-amber-50 font-mono font-medium text-xl flex flex-row items-center">
-              <span className=" underline">Rating</span>
-              : 369
+              <span className=" underline">Rating</span>: 369
             </Card>
 
             <Card className="text-white bg-zinc-900 border-2 px-10 border-amber-50">
---
+++ src/components/general/article-set.jsx
@@ -0,0 +1,161 @@
+import { Card } from "../ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function ArticleSet() {
+  return (
+    <div>
+      <Card className="text-white bg-zinc-900 border-2 px-10 border-amber-50 font-mono font-medium text-xl flex flex-row items-center">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Topics</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead className="text-right">Difficulty</TableHead>
+            </TableRow>
+          </TableHeader>
+					{/* Demo data */}
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="font-medium">1</TableCell>
+              <TableCell>P1</TableCell>
+              <TableCell className="text-right">900</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </Card>
+    </div>
+  );
+}
